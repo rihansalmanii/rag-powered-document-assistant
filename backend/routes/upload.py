42d4f44be72f9e626df2cc7cc7179849a695adfa
rihansalmanii <salmanirihan76@gmail.com>
2026-07-07 00:41:44 +0530

    text chunking implement

--- backend/routes/upload.py
+++ backend/routes/upload.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, UploadFile, File
 from services.pdf_services import extract_text
+from services.chunking import chunk_text
+
 
 router = APIRouter()
 
@@ -8,9 +10,13 @@ async def upload_pdf(file: UploadFile = File(...)):
     try:
         text = extract_text(file)
 
+        chunks = chunk_text(text)
+
         return {
             "filename": file.filename,
             "text_length": len(text),
+            "chunks_length": len(chunks),
+            "first_chunk": chunks[0], 
             "preview": text[:500]
         }
 
